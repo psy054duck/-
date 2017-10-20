2cda555bce3828f062d4a4558f78584e683920a2
psy054duck <wangchlin0@163.com>
2017-10-20 09:18:12 +0800

    exception

--- Postfix/src/Postfix.java
+++ Postfix/src/Postfix.java
@@ -2,16 +2,28 @@ import java.io.*;
 
 class Parser {
 	static int lookahead;
+	static int previous;
+	boolean isWrong = false;
 	Scanner scanner;
+	String res;
 
 	public Parser() throws IOException, Error {
 		scanner = new Scanner();
-		lookahead = scanner.getNextToken();
+		try {
+			lookahead = scanner.getNextToken();
+		} catch (Error e) {
+			System.out.println(e.getMessage());
+		}
+		res = "";
 	}
 
 	void expr() throws IOException, Error {
 		term();
 		rest();
+		if (! isWrong) {
+			System.out.print(res);
+			isWrong = false;
+		}
 	}
 
 	void rest() throws IOException, Error {
@@ -19,30 +31,110 @@ class Parser {
 			if (lookahead == '+') {
 				match('+');
 				term();
-				System.out.write('+');
+				res += "+";
 			} else if (lookahead == '-') {
 				match('-');
 				term();
-				System.out.write('-');
-			} else {
+				res += "-";
+			} else if (lookahead == '\n') {
 				break;
+			} else {
+				missOperator();
 			}
 		}
 	}
 
 	void term() throws IOException, Error {
-		if (Character.isDigit((char)lookahead)) {
-			System.out.write((char)lookahead);
-			match(lookahead);
-		} else throw new Error("syntax error");
+		while (true) {
+			if (Character.isDigit((char) lookahead)) {
+				res += (char) lookahead;
+				match(lookahead);
+				break;
+			} else {
+				missDigit();
+				if (lookahead == '\n') {
+					break;
+				}
+			}
+		}
 	}
 
 	void match(int t) throws IOException, Error {
 		if (lookahead == t) {
-			lookahead = scanner.getNextToken();
+			try {
+				previous = lookahead;
+				lookahead = scanner.getNextToken();
+			} catch (Error e) {
+				isWrong = true;
+				lookahead = 'e';
+				System.out.println(e.getMessage());
+			}
 		} else {
+			isWrong = true;
 			throw new Error("syntax error");
 		}
+	}
+
+	private void missDigit() {
+		String errors = "";
+		isWrong = true;
+		System.out.println("Syntax error: expected 'digit' token");
+		System.out.print("\t" + scanner.getInput() + "\n\t");
+		System.out.print(nspace(scanner.getCount()-1));
+		System.out.print("^");
+		int cnt = -1;
+		previous = lookahead;
+		while (lookahead != '\n' && ! Character.isDigit((char) lookahead)) {
+			++cnt;
+			try {
+				lookahead = scanner.getNextToken();
+			} catch (Error e) {
+				lookahead = 'e';
+				errors += e.getMessage() + "\n";
+			}
+		}
+		for (int i = 0; i < cnt; ++i) {
+			System.out.print("~");
+		}
+		System.out.print("\n");
+		if (! errors.isEmpty()) {
+			System.out.print(errors);
+		}
+	}
+
+	private void missOperator() {
+		String errors = "";
+		isWrong = true;
+		System.out.println("Syntax error: expected '+' or '-' token");
+		System.out.print("\t" + scanner.getInput() + "\n\t");
+		System.out.print(nspace(scanner.getCount()-1));
+		System.out.print("^");
+		int cnt = -1;
+		previous = lookahead;
+		while (lookahead != '\n' && lookahead != '+' && lookahead != '-') {
+			++cnt;
+			try {
+				lookahead = scanner.getNextToken();
+			} catch (Error e) {
+				lookahead = 'e';
+				errors += e.getMessage() + "\n";
+			}
+		}
+		for (int i = 0; i < cnt; ++i) {
+			System.out.print("~");
+		}
+		System.out.print("\n");
+		if (! errors.isEmpty()) {
+			System.out.print(errors);
+		}
+	}
+
+	private String nspace(int n) {
+		String res = "";
+		for (int i = 0; i < n; ++i) {
+			res += " ";
+		}
+		return res;
 	}
 }
 
@@ -59,24 +151,31 @@ class Scanner {
 		if (cnt == buffer.length()) return '\n';
 		char ch = buffer.charAt(cnt++);
 		if (! check(ch)) {
-			System.out.printf("\n\t%s\n", buffer);
-			System.out.print('\t');
-			nspace(cnt-1);
-			System.out.print("^\n");
-			System.out.println("Lexical error: '+', '-' and digits are expected");
-			throw new Error();
+			String errorMessage = "Lexical error: '+', '-' or digits are expected\n";
+			errorMessage += "\t" + buffer + "\n\t" + nspace(cnt-1) + "^";
+			throw new Error(errorMessage);
 		}
 		return ch;
+	}
+
+	public String getInput() {
+		return buffer;
+	}
+
+	public int getCount() {
+		return cnt;
 	}
 
 	private boolean check(char ch) throws Error {
 		return (ch == '+' || ch == '-' || Character.isDigit((char) ch));
 	}
 
-	private void nspace(int n) {
+	private String nspace(int n) {
+		String res = "";
 		for (int i = 0; i < n; ++i) {
-			System.out.print(' ');
+			res += " ";
 		}
+		return res;
 	}
 }
 
@@ -84,6 +183,6 @@ public class Postfix {
 	public static void main(String[] args) throws IOException {
 		System.out.println("Input an infix expression and output its postfix notation:");
 		new Parser().expr();
-		System.out.println("\nEnd of program.");
+		System.out.println("End of program.");
 	}
 }
